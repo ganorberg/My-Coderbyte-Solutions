14c73686df4df0fc6fee09eb785b5595ff6174a8
ganorberg <george.norberg@gmail.com>
2016-08-18 00:01:21 -0700

    Create 13: Word Count.js

---
+++ Easy/13: Word Count.js
@@ -0,0 +1,9 @@
+function WordCount(str) { 
+  
+  // Split string by spaces
+  var arr = str.split(' ');
+  
+  // Length of array represents total number of words
+  return arr.length;
+         
+}
