d76b6cec9dbb000c82e4e3b2edaa28e61672174a
ganorberg <george.norberg@gmail.com>
2016-08-17 23:54:08 -0700

    Create 10: Alphabet Soup.js

---
+++ Easy/10: Alphabet Soup.js
@@ -0,0 +1,12 @@
+function AlphabetSoup(str) { 
+
+  // Split string into array of letters
+  var arr = str.split('');
+  
+  // Sort letters by ascending order of Unicode
+  arr.sort(function(a, b) { return a.charCodeAt(0) - b.charCodeAt(0); });
+  
+  // Join letters into string
+  return arr.join('');
+         
+}
