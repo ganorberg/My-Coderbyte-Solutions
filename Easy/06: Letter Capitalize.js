6b4098728ba638fa67ad2a3bce1a731a88025eab
ganorberg <george.norberg@gmail.com>
2016-08-18 01:15:54 -0700

    Update 06: Letter Capitalize.js

--- Easy/06: Letter Capitalize.js
+++ Easy/06: Letter Capitalize.js
@@ -1,25 +1,19 @@
 function LetterCapitalize(str) { 
-  //split into words
-  //loop through each word
-    //store first letter and rest of word
-    //capitalize first letter
-    //join first letter with rest of word
-    //join words with space
-  
+
   // Split string into words array 
-  var words = str.split(' ');
+  let words = str.split(' ');
   
   // Create solution storage
-  var answer = [];
+  let answer = [];
   
   // Loop through each word
-  for (var i = 0; i < words.length; i++) {
+  for (let i = 0; i < words.length; i++) {
   
       // Current word
-      var word = words[i];
+      let word = words[i];
       
       // Create new word by capitalizing first letter then concatenating with rest of word
-      var newWord = word[0].toUpperCase() + word.slice(1, word.length + 1);
+      let newWord = word[0].toUpperCase() + word.slice(1, word.length + 1);
       
       // Push newly capitalized word to answer array
       answer.push(newWord);
