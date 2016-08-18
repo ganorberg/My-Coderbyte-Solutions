bdac4540e28b8b73e393ceb0b5920bfbec0c6f08
ganorberg <george.norberg@gmail.com>
2016-08-18 01:12:29 -0700

    Update 13: Word Count.js

--- Easy/13: Word Count.js
+++ Easy/13: Word Count.js
@@ -1,7 +1,7 @@
 function WordCount(str) { 
   
   // Split string by spaces
-  var arr = str.split(' ');
+  let arr = str.split(' ');
   
   // Length of array represents total number of words
   return arr.length;
