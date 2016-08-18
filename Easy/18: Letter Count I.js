413c44217171486cc41dbefbea711e1f3c825ce5
ganorberg <george.norberg@gmail.com>
2016-08-18 01:09:51 -0700

    Update 18: Letter Count I.js

--- Easy/18: Letter Count I.js
+++ Easy/18: Letter Count I.js
@@ -1,21 +1,21 @@
 function LetterCountI(str) { 
   
   // Create object to store word objects, which will contain key-value pairs of letter: count and highest: #
-  var table = {};
+  let table = {};
   
   // Break string into separate words
-  var arr = str.split(' ');
+  let arr = str.split(' ');
   
   // Loop through words
-  for (var i = 0; i < arr.length; i++) {
-      var word = arr[i];
+  for (let i = 0; i < arr.length; i++) {
+      let word = arr[i];
       
       // Give each word a 'highest' property to tell us its highest letter count
       table[word] = {highest: 1};
       
       // Loop through letters of each word
-      for (var j = 0; j < word.length; j++) {
-          var letter = word[j];
+      for (let j = 0; j < word.length; j++) {
+          let letter = word[j];
           
           // If letter doesn't exist in word object, create it with a value of 1
           if (table[word][letter] === undefined) {
@@ -32,10 +32,10 @@ function LetterCountI(str) {
   }
   
   // Create storage for word with highest count
-  var answer = {word: null, highest: 1};
+  let answer = {word: null, highest: 1};
   
   // Loop through words in table to track highest count
-  for (var w in table) {
+  for (let w in table) {
       
       // If current word's highest count is greater than the overall highest, replace highest and store associated word
       if (table[w]['highest'] > answer['highest']) {
