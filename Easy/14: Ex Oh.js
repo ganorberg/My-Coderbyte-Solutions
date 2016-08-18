69da842c795e8d6a9e9172b376762573236be388
ganorberg <george.norberg@gmail.com>
2016-08-18 01:12:02 -0700

    Update 14: Ex Oh.js

--- Easy/14: Ex Oh.js
+++ Easy/14: Ex Oh.js
@@ -1,11 +1,11 @@
 function ExOh(str) { 
   
   // Establish counters for 'x' and 'o'
-  var x = 0;
-  var o = 0;
+  let x = 0;
+  let o = 0;
   
   // Loop through string
-  for (var i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
   
       // Add 1 to respective counter each time an 'x' or 'o' is found
       if (str.charAt(i) === 'x') {
