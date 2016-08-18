7e3d773f49a04f3efc13f192303b589f27291fea
ganorberg <george.norberg@gmail.com>
2016-08-17 23:58:22 -0700

    Create 11: AB Check.js

---
+++ Easy/11: AB Check.js
@@ -0,0 +1,23 @@
+function ABCheck(str) { 
+
+  // Loop through word  
+  for (var i = 0; i < str.length; i++) {
+  
+      // If letter equals 'a', check 3 positions in front and behind for 'b'
+      if (str.charAt(i) === 'a') {
+          if (str.charAt(i+4) === 'b' || str.charAt(i-4) === 'b') {
+              return true;
+          }
+      }
+      
+      // If letter equals 'b', check 3 positions in front and behind for 'a'
+      if (str.charAt(i) === 'b') {
+          if (str.charAt(i+4) === 'a' || str.charAt(i-4) === 'a') {
+              return true;
+          }
+      }
+  }
+  
+  // If condition doesn't pass through entire word, then 'a' and 'b' are not 3 spaces apart.
+  return false;
+}
