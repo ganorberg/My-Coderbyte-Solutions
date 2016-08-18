8d39a78c93d4cbba9e122fe36ca40f5f92864b82
ganorberg <george.norberg@gmail.com>
2016-08-18 01:16:10 -0700

    Update 05: Simple Adding.js

--- Easy/05: Simple Adding.js
+++ Easy/05: Simple Adding.js
@@ -1,10 +1,10 @@
 function SimpleAdding(num) { 
   
   // Create storage for sum
-  var sum = 0;
+  let sum = 0;
   
   // Add all values up to and including num
-  for (var i = 1; i <= num; i++) {
+  for (let i = 1; i <= num; i++) {
     sum += i;  
   }
   
