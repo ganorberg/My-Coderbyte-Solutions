2dc27ed5894679964d211d798ce58d612ff0b48c
ganorberg <george.norberg@gmail.com>
2016-08-18 01:11:37 -0700

    Update 15: Palindrome.js

--- Easy/15: Palindrome.js
+++ Easy/15: Palindrome.js
@@ -1,13 +1,13 @@
 function Palindrome(str) { 
 
   // Remove spaces by splitting. Then, join characters together and convert to array of characters without spaces.
-  var arr = str.split(' ').join('').split('');
+  let arr = str.split(' ').join('').split('');
   
   // Reverse characters with native method
-  var reversed = arr.reverse().join('');
+  let reversed = arr.reverse().join('');
   
   // Create original string without spaces
-  var noSpaces = str.split(' ').join('');
+  let noSpaces = str.split(' ').join('');
   
   // After removing all spaces, return Boolean for whether the original string equals the reversed string
   return noSpaces === reversed;
