1b33190581f2c4c3dc919e84112268ea465aebb1
ganorberg <george.norberg@gmail.com>
2016-08-18 01:08:01 -0700

    Create 21: Counting Minutes I.js

---
+++ Easy/21: Counting Minutes I.js
@@ -0,0 +1,45 @@
+function CountingMinutesI(str) { 
+  
+  // Split into beginning and end times
+  let times = str.split('-');
+  let startTime = times[0];
+  let endTime = times[1];
+  
+  // Create function that calculates minutes from midnight for each time
+  function minutesFromMidnight(time) {
+    
+    // Split by colon to access hours and minutes
+    let split = time.split(':');
+    let hours = parseInt(split[0]);
+    let minutes = parseInt(split[1]);
+    
+    // Calculate total minutes temporarily
+    let totalMinutes = hours * 60 + minutes;
+    
+    // If midnight, subtract 720 minutes
+    if (time.includes('a') && hours === 12) {
+       totalMinutes -= 12 * 60;
+    }
+    
+    // If pm but NOT noon, add 720 minutes.
+    if (time.includes('p') && hours !== 12) {
+       totalMinutes += 12 * 60;
+    }
+    
+    // Function returns total minutes with exceptions included.
+    return totalMinutes;
+  }
+  
+  // Calculate minutes from midnight for each time
+  let startMinutesFromMidnight = minutesFromMidnight(startTime);
+  let endMinutesFromMidnight = minutesFromMidnight(endTime);
+  
+  // If ends same day, return difference
+  if (endMinutesFromMidnight > startMinutesFromMidnight) {
+     return endMinutesFromMidnight - startMinutesFromMidnight;
+     
+  // If ends next day, subtract difference from 1440 minutes (total minutes in a day)
+  } else {
+     return 1440 - (startMinutesFromMidnight - endMinutesFromMidnight);
+  }
+}
