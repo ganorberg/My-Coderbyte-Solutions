361c74444af274a962ca5d9d5e118c3b9eebb290
ganorberg <george.norberg@gmail.com>
2016-08-18 01:12:54 -0700

    Update 12: Vowel Count.js

--- Easy/12: Vowel Count.js
+++ Easy/12: Vowel Count.js
@@ -1,13 +1,13 @@
 function VowelCount(str) { 
   
   // Make all letter lowercase for conditional check in loop
-  var str = str.toLowerCase();
+  let str = str.toLowerCase();
   
   // Create counter storage
-  var counter = 0;
+  let counter = 0;
   
   // Loop through string
-  for (var i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
   
       // If character is a vowel, add 1 to counter
       if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
@@ -17,4 +17,5 @@ function VowelCount(str) {
   
   // Counter represents total number of vowels in string
   return counter;
+  
 }
