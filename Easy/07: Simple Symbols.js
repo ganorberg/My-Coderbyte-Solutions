08dbb2547d2fa2dcb41c05105e6cbc2dfcf7e7f4
ganorberg <george.norberg@gmail.com>
2016-08-17 23:48:21 -0700

    Create 07: Simple Symbols.js

---
+++ Easy/07: Simple Symbols.js
@@ -0,0 +1,24 @@
+function SimpleSymbols(str) { 
+
+  // Convert alphabetic characters to lowercase for unicode check
+  var str = str.toLowerCase();
+  
+  // Loop through string
+  for (var i = 0; i < str.length; i++) {
+  
+      // If alphabetic character...
+      if (str.charCodeAt(i) > 96 && str.charCodeAt(i) < 123) {
+      
+          // ...and if doesn't have a plus sign to the left or right...
+          if (str[i-1] !== '+' || str[i+1] !== '+') {
+          
+              // ... we know that not all alphabetic characters have a plus sign to left and right.
+              return false;
+          }
+      }
+  }
+  
+  // If loop never finds an alphabetic character without plus signs to the left or right,
+  // then all alphabetic characters have a plus sign to the left and right!
+  return true;
+}
