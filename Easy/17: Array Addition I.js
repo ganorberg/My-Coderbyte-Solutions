c84c9fb8d9556f5b1e0aba501b77a1b76c663a36
ganorberg <george.norberg@gmail.com>
2016-08-18 00:22:26 -0700

    Create 17: Array Addition I.js

---
+++ Easy/17: Array Addition I.js
@@ -0,0 +1,32 @@
+function ArrayAdditionI(arr) { 
+  
+    
+  
+  // Calculate highest value by reducing array such that only the larger of each pair of values is returned
+  var highest = arr.reduce(function(p, v) { return v > p ? v : p; });
+  
+  // Filter out highest value by keeping all values less than the highest value
+  var smallArr = arr.filter(function(val) { return val < highest; });
+  
+  // Found this subsetsum function from lumberjack87 in Coderbyte forum
+  // Uses recursive template from Eloquent Javascript Ch.3 to search all possible combinations via branching
+  function subsetsum(target, array) {
+    
+    // Base case when array is emptied
+    if (array.length === 0) {
+        return target === 0;
+    }
+    
+    // First term is stored
+    var n = array[0];
+    
+    // Only values after the first term are saved
+    array = array.slice(1);
+    
+    // Call function with same target and new array OR subtract first value from target, then search new array 
+    return subsetsum(target, array) || subsetsum(target - n, array);
+  }
+  
+  // Recursive magic finds combination of values in smallArr that add to our highest value
+  return subsetsum(highest, smallArr);
+}
