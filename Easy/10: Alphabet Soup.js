72ce98608aa423435623af0143d3a682df32e39a
ganorberg <george.norberg@gmail.com>
2016-08-18 01:14:41 -0700

    Update 10: Alphabet Soup.js

--- Easy/10: Alphabet Soup.js
+++ Easy/10: Alphabet Soup.js
@@ -1,12 +1,11 @@
 function AlphabetSoup(str) { 
 
   // Split string into array of letters
-  var arr = str.split('');
+  let arr = str.split('');
   
   // Sort letters by ascending order of Unicode
   arr.sort(function(a, b) { return a.charCodeAt(0) - b.charCodeAt(0); });
   
   // Join letters into string
   return arr.join('');
-         
 }
