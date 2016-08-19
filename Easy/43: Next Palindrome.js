f6133d0f5ff8c95ea73dbad19d9193c328b787d1
ganorberg <george.norberg@gmail.com>
2016-08-19 03:54:29 -0700

    Determine the next largest palindrome

--- Easy/43: Next Palindrome.js
+++ Easy/43: Next Palindrome.js
@@ -1,27 +1,27 @@
 function NextPalindrome(num) { 
-  
+    
   // Create check for palindrome
   function isPalindrome(str) {
-    return str.split(' ').join('').split('').reverse().join('') === str.split(' ').join('');
+    return str.split('').reverse().join('') === str;
   }
   
   // Start checking at next number
   num = num + 1;
   
   // Increase number until find palindrome
-  while (isPalindrome(num) === false) {
-    num++; 
+  while (isPalindrome(num.toString()) === false) {
+    num++;
   }
   
-  // If loop exits, we have our next palindrome!
+  // When loop exits, we have our next palindrome!
   return (num);
 }
 
 WITHOUT COMMENTS:
 
 function NextPalindrome(num) { 
-  function isPalindrome(str) {return str.split(' ').join('').split('').reverse().join('') === str.split(' ').join('');}
+  function isPalindrome(str) {return str.split('').reverse().join('') === str;}
   num = num + 1;
-  while (isPalindrome(num) === false) {num++;}
+  while (isPalindrome(num.toString()) === false) {num++;}
   return (num);
 }
