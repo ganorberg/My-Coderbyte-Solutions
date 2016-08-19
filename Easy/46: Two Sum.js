e5afeca3e9e5b006211d7e07414092a789ba03cb
ganorberg <george.norberg@gmail.com>
2016-08-19 04:10:47 -0700

    Determine if two elements can sum to larger number

---
+++ Easy/46: Two Sum.js
@@ -0,0 +1,44 @@
+function TwoSum(arr) {
+  
+  // Remove and store first integer
+  let target = arr.shift();
+  
+  // Store length for loops
+  const LEN = arr.length;
+  
+  // Create storage array for sum pair subarrays
+  let answer = [];
+
+  // Add each term to rest of terms one by one
+  for (let i = 0; i < LEN; i++) {
+    for (let j = i + 1; j < LEN; j++) {
+      
+      // If both integers add to target, push as subarray then end inner loop since pair was found
+      if (arr[i] + arr[j] === target) {
+        answer.push([arr[i],arr[j]]);
+        break;
+      }
+    }
+  }
+  
+  // If subarray pairs stored, join with space. Otherwise, return -1.
+  return (answer.length > 0) ? answer.join(' ') : -1;
+}
+
+WITHOUT COMMENTS:
+
+function TwoSum(arr) {
+  let target = arr.shift();
+  const LEN = arr.length;
+  let answer = [];
+
+  for (let i = 0; i < LEN; i++) {
+    for (let j = i + 1; j < LEN; j++) {
+      if (arr[i] + arr[j] === target) {
+        answer.push([arr[i],arr[j]]);
+        break;
+      }
+    }
+  }
+  return (answer.length > 0) ? answer.join(' ') : -1;
+}
