97afcb15b95a41abcac72aa78c47a6742c62fc4c
George Norberg <george.norberg@gmail.com>
2016-08-20 16:49:36 -0700

    Perform bitwise AND operation on two binary numbers

--- Easy/47: Bitwise Two.js
+++ Easy/47: Bitwise Two.js
@@ -7,7 +7,7 @@ function BitwiseTwo(strArr) {
     for (var i = 0; i < strArr[0].length; i++) {
     
         // If either respective digit is a 0, then 0 is pushed instead of 1
-        if (strArr[0][i] == 0 || strArr[1][i] == 0) {
+        if (strArr[0][i] === '0' || strArr[1][i] === '0') {
             answer.push('0');
         } else {
             answer.push('1');
@@ -15,21 +15,5 @@ function BitwiseTwo(strArr) {
     }
     
     // Output as string
-    return answer.join('');
-}
-
-
-WITHOUT COMMENTS:
-
-function BitwiseTwo(strArr) { 
-    var answer = [];
-    
-    for (var i = 0; i < strArr[0].length; i++) {
-        if (strArr[0][i] == 0 || strArr[1][i] == 0) {
-            answer.push('0');
-        } else {
-            answer.push('1');
-        }
-    }
     return answer.join('');
 }
