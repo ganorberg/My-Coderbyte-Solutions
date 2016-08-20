dc3b5e64cf023e654d7d02b820fc86167d10377d
ganorberg <george.norberg@gmail.com>
2016-08-20 02:41:40 -0700

    Swap the case of each character in the string

--- Easy/24: Swap Case.js
+++ Easy/24: Swap Case.js
@@ -1,18 +1,16 @@
-// Objective: Swap the case of each character in the string.
-
 function SwapCase(str) { 
   
   // Create array to modify in place
-  chars = str.split("");
+  let chars = str.split("");
   
   // Loop through array of characters
   for (let i = 0; i < chars.length; i++) {
   
-      // If current value is uppercase, make lowercase.
+      // If current value is uppercase, make lowercase
       if (chars[i] === chars[i].toUpperCase()) {
           chars[i] = chars[i].toLowerCase();
           
-      // If current value is lowercase, make uppercase.
+      // If current value is lowercase, make uppercase
       } else if (chars[i] === chars[i].toLowerCase()) {
           chars[i] = chars[i].toUpperCase();
       }
