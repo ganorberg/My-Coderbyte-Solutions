0e690159c1e3ca56c24efa14b9ae08042d9cc694
ganorberg <george.norberg@gmail.com>
2016-08-20 01:24:09 -0700

    Determine whether one of two numbers is greater

--- Easy/08: Check Nums.js
+++ Easy/08: Check Nums.js
@@ -1,5 +1,5 @@
 function CheckNums(num1,num2) { 
   
-  // If the numbers are equal, return -1. If not, return Boolean for whether num2 is greater than num1.
+  // If the numbers are equal, return -1. Otherwise, return Boolean for whether num2 is greater than num1.
   return (num1 === num2) ? -1 : num2 > num1;
 }
