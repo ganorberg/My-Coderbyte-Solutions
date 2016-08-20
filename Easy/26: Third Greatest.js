a6658c35fa089631f06635d49484534e92acbd8e
ganorberg <george.norberg@gmail.com>
2016-08-20 02:53:00 -0700

    Determine the third largest string within an array

--- Easy/26: Third Greatest.js
+++ Easy/26: Third Greatest.js
@@ -1,11 +1,8 @@
-// Objective: Determine the third largest string within an array.
-
 function ThirdGreatest(strArr) { 
     
-    // Sort array in descending order of length of each string
+    // Sort array in descending order of each string's length
     var arr = strArr.sort(function(a,b){ return b.length - a.length });
     
-    // Third term now equals third largest term
+    // Third term now equals third largest term!
     return arr[2]; 
-         
 }
