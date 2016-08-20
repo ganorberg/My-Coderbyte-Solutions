3b31d15e11790e350909195e2934a4e525a236b5
ganorberg <george.norberg@gmail.com>
2016-08-20 01:33:30 -0700

    Determine if two characters are separated a specific way

--- Easy/11: AB Check.js
+++ Easy/11: AB Check.js
@@ -9,15 +9,8 @@ function ABCheck(str) {
               return true;
           }
       }
-      
-      // If letter equals 'b', check 3 positions in front and behind for 'a'
-      if (str.charAt(i) === 'b') {
-          if (str.charAt(i+4) === 'a' || str.charAt(i-4) === 'a') {
-              return true;
-          }
-      }
   }
   
-  // If condition doesn't pass through entire word, then 'a' and 'b' are not 3 spaces apart.
+  // If loop fails to return true, then 'a' and 'b' are not 3 spaces apart!
   return false;
 }
