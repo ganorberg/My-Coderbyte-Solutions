d363da6a997a6f6e7883c43e413d2cc4ff862a11
ganorberg <george.norberg@gmail.com>
2016-08-20 01:36:50 -0700

    Determine how many words a sentence contains

--- Easy/13: Word Count.js
+++ Easy/13: Word Count.js
@@ -1,8 +1,8 @@
 function WordCount(str) { 
   
-  // Split string by spaces
+  // Split string by spaces to separate words
   let arr = str.split(' ');
   
-  // Length of array represents total number of words
+  // Length of array represents total number of words!
   return arr.length;
 }
