e8ee14fa77d7c4a6415b571ac9e587e53b40452f
ganorberg <george.norberg@gmail.com>
2016-08-20 01:18:55 -0700

    Determine if certain characters are in correct positions

--- Easy/07: Simple Symbols.js
+++ Easy/07: Simple Symbols.js
@@ -1,6 +1,6 @@
 function SimpleSymbols(str) { 
 
-  // Convert alphabetic characters to lowercase for unicode check
+  // Convert alphabetic characters to lowercase for Unicode check
   let str = str.toLowerCase();
   
   // Loop through string
@@ -9,16 +9,16 @@ function SimpleSymbols(str) {
       // If alphabetic character...
       if (str.charCodeAt(i) > 96 && str.charCodeAt(i) < 123) {
       
-          // ...and if doesn't have a plus sign to the left or right...
+          // ...and it doesn't have a plus sign to the left or right...
           if (str[i-1] !== '+' || str[i+1] !== '+') {
           
-              // ... we know that not all alphabetic characters have a plus sign to left and right.
+              // ... we know that at least one alphabetic character lacks a plus sign to the left or right!
               return false;
           }
       }
   }
   
   // If loop never finds an alphabetic character without plus signs to the left or right,
-  // then all alphabetic characters have a plus sign to the left and right!
+  // then all alphabetic characters must have a plus sign to the left and right!
   return true;
 }
