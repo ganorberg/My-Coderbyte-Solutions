d2d1fbf33048959f3a6794234fc3577011cc46b7
ganorberg <george.norberg@gmail.com>
2016-08-20 02:12:09 -0700

    Divide two numbers and return in certain format

--- Easy/20: Division Stringified.js
+++ Easy/20: Division Stringified.js
@@ -1,7 +1,5 @@
 function DivisionStringified(num1,num2) { 
   
-  // Use native toLocaleString method to standardize number
-  // Be sure to convert string into number and remove decimals first
+  // Use native toLocaleString method to standardize numbers. No decimals included here.
   return Number((num1 / num2).toFixed(0)).toLocaleString(); 
-         
 }
