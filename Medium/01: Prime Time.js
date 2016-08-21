ea379251f6881a2d6438111ade7379c1c448c66c
George Norberg <george.norberg@gmail.com>
2016-08-21 00:12:53 -0700

    Determine if an argument is a prime number

---
+++ Medium/01: Prime Time.js
@@ -0,0 +1,18 @@
+function PrimeTime(num) { 
+  
+  // Integers less than 2 cannot be prime
+  if (num < 2) {
+    return false;
+  }
+  
+  // Brute force method checks if our number is divisible by any number until its square root.
+  // This ends the loop before iterating over factors a second time.
+  for (i = 2; i < Math.sqrt(num); i++) {
+      if (num % i === 0) {
+          return false;
+      }
+  }
+  
+  // If no integers divide into our number (excluding values less than 2), then we have a prime number!
+  return true;
+}
