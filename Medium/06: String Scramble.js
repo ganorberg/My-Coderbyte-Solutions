072d892bff9e44544059c106a3c260bbb8f030c8
George Norberg <george.norberg@gmail.com>
2016-08-21 01:59:46 -0700

    Update 06: String Scramble.js

--- Medium/06: String Scramble.js
+++ Medium/06: String Scramble.js
@@ -6,15 +6,16 @@ function StringScramble(str1,str2) {
     // Loop through second string
     for (let i = 0; i < str2.length; i++) {
     
-        // If character of string 2 exists in string 1, we delete that char from array 1.
-        // Any time a value is not found, we know string 2 cannot be rearranged into string 1.
+        // When a value is not found, we know string 2 cannot be rearranged into string 1.
         if (!arr1.includes(str2[i])) {
             return false;
+            
+        // For every matching char, we remove that char from array 1 to track duplicates
         } else {
             arr1.splice(arr1.indexOf(str2[i]), 1);
         }
     }
     
-    // String 2 can be rearranged into string 1 if we were able to splice every letter out of array 1!
+    // If we were able to splice every letter out of array 1, then string 2 can be rearranged into string 1!
     return true;
 }
