b0b9b155dee1284218938a703aa72b76ef73330a
George Norberg <george.norberg@gmail.com>
2016-08-21 00:57:03 -0700

    Determine if an argument is a prime number

--- Medium/01: Prime Time.js
+++ Medium/01: Prime Time.js
@@ -6,7 +6,7 @@ function PrimeTime(num) {
   }
   
   // Brute force method checks if our number is divisible by any number until its square root.
-  // This ends the loop before iterating over factors a second time.
+  // This ends the loop before iterating over factors a second time (assuming we had ended the loop at num instead).
   for (i = 2; i <= Math.sqrt(num); i++) {
       if (num % i === 0) {
           return false;
