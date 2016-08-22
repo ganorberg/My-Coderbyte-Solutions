0f88b4514b7fc7d1caf08670152a41821f464937
George Norberg <george.norberg@gmail.com>
2016-08-22 02:28:03 -0700

    Determine if brackets in a string are correctly matched up

---
+++ Medium/22: Bracket Matcher.js
@@ -0,0 +1,26 @@
+function BracketMatcher(str) {
+    
+    // Track number of left parens vs right parens
+    let counter = 0;
+    
+    // Loop through string
+    for (let i = 0; i < str.length; i++) {
+        
+        // Increase counter by 1 if we find a left parens
+        if (str[i] === "(") {
+            counter++;
+            
+        // Decrease counter by 1 if we find a right parens
+        } else if (str[i] === ")") {
+            counter--;
+        }
+        
+        // If counter drops below 0, a right parens has no left match!
+        if (counter < 0) {
+            return 0;
+        }
+    }
+    
+    // If all pairs matched, return 1. Otherwise, return 0.
+    return counter === 0 ? 1 : 0;
+}
