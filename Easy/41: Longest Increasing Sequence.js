2bfe6d5223f1fd27b86a5b39080d6edebb4a3709
George Norberg <george.norberg@gmail.com>
2016-09-01 17:04:24 -0700

    Compute LIS of an array of integers
    Shoutout to jchappypig for the original form of this brilliant solution!

--- Easy/41: Longest Increasing Sequence.js
+++ Easy/41: Longest Increasing Sequence.js
@@ -1,46 +1,44 @@
 function LongestIncreasingSequence(arr) {
-    let len = arr.length;
-    let arrHolder = [];
+  
+  /* 
+   * First, create a function that recursively builds increasing sequences (IS).
+   * The first parameter, start, represents the first value of each subarray.
+   * The second parameter, arr, represents the rest of the subarray's values. 
+   */
+   
+  function findLongestFromStart(start, arr) {
+      
+    // When the array is emptied, return 1 as base case.
+    if (arr.length === 0) return 1;
     
-    //evaluate each possible combination of numbers
-    for (let i = Math.pow(2, len); i < Math.pow(2, len + 1); i++) {
+    // Build array to capture lengths of all IS.
+    let listLength = arr.map((elem, index) => {
         
-        //numArray is a binary digit, the 0s and 1s representing whether to include
-        //a number or not in the combination array.  There will be 2 ^ n combinations
-        //to get leading zeros, use numbers from 2^n to 2^n+1, then slice off the leading 1
-        let numArray = i.toString(2).slice(1).split('');
-        
-        //hold the selected numbers in the newSeq array
-        newSeq = [];
-        
-        //populate the newSeq array
-        arr.forEach((val, ind) => {
-            if (numArray[ind] === '1') {
-            newSeq.push(val);
-            }
-        });
-        
-        // include the newSeq array into arrHolder
-        arrHolder.push(newSeq);
-    }
+      // When we encounter a larger term, build all subarrays from that value.
+      // Add 1 to represent increased length caused by that term.
+      if (elem > start) {
+        return 1 + findLongestFromStart(arr[index], arr.slice(index + 1));
+      }
     
-    //take out all the arrays that are not ascending (use the ascend() to determine)
-    arrHolder = arrHolder.filter(val => ascend(val));
-    
-    //replace each passing array with its length
-    let arrLength = arrHolder.map(val => val.length);
-    
-    //return the largest length value
-    return Math.max(...arrLength);
-}
-
-
-function ascend(inputArr) {
-    let arrlen = inputArr.length;
-    return inputArr.every((val, ind) => {
-        if (ind < arrlen - 1) {
-            return val < inputArr[ind + 1];
-        }
-        return true;
+      // If we encounter smaller terms, we stop their sequence at length of 1.
+      return 1;
     });
+  
+  // LIS of this array is represented by the max value found in listLength
+  return Math.max(...listLength);
+  }
+
+  // Create storage for LIS of all possible IS arrays
+  let max = 0;
+  
+  // Loop through input array to calculate length of
+  // all possible IS and update max value accordingly.
+  arr.forEach((elem, index) => {
+    let longest = findLongestFromStart(elem, arr.slice(index + 1));
+    
+    if (longest > max) max = longest;
+  });
+  
+  // Output represents longest increasing sequence!
+  return max;
 }
