160c3b6436d91ee3911658664187bae4c8ee901e
George Norberg <george.norberg@gmail.com>
2016-09-01 17:17:15 -0700

    Determine if you can create a palindrome from a list of numbers

--- Easy/50: Palindrome Creator.js
+++ Easy/50: Palindrome Creator.js
@@ -37,35 +37,3 @@ function PalindromeCreator(str) {
     // If no palindrome was found:
     return "not possible";
 }
-
-
-WITHOUT COMMENTS:
-
-function PalindromeCreator(str) {
-    if (str === str.split("").reverse().join("")) {
-        return "palindrome";
-    }
-    var length = str.length;
-    str = str.split("");
-    for (let i = 0; i < length; i++) {
-        let testChar = str.splice(i,1).join("");
-        if (str.join("") === str.reverse().join("")){
-            return testChar;
-        }
-        str.reverse().splice(i,0,testChar);
-    }
-    for (let i = 0; i < length; i++) {
-        let testChar = str.splice(i,1).join("");
-        if (length > 4) {
-          for (let j = 0; j < str.length; j++) {
-              let testChar2 = str.splice(j,1).join("");
-              if (str.join("") === str.reverse().join("")){
-                  return testChar+ "" + testChar2;
-                }
-                str.reverse().splice(j,0,testChar2);
-          }
-        }
-        str.splice(i,0,testChar);
-    }
-    return "not possible";
-}
