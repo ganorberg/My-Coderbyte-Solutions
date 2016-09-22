55b14bcf3718fae9c8e7ae80d3dec4ce4027fd68
George Norberg <george.norberg@gmail.com>
2016-09-22 14:30:28 -0700

    Determine if numbers in an array can add up to a certain number

--- Medium/08: Array Addition.js
+++ Medium/08: Array Addition.js
@@ -1,28 +1,30 @@
 function ArrayAddition(arr) { 
   
-  // sort array ascending
-  arr = arr.sort( function(a,b){return a - b});
+  // Sort array in ascending order
+  let array = arr.sort((a,b) => a - b);
   
-  var max = arr.pop();
+  // Extract target
+  let target = array.pop();
   
+  // Recursively search all possible combinations
   function lookForSolution(){
-    function search(sum,i) {
-      if ( sum == max ) {
+    function search(sum, i) {
+      if (sum == target) {
         return true;
       }
-      else if ( sum > max || i == arr.length ) {
+      else if (sum > target || i === array.length) {
         return null;
       }
       else {
-        return search(sum + arr[i],i + 1) ||
-               search(sum,i + 1);
+        return search(sum + array[i], i + 1) ||
+               search(sum, i + 1);
       }
     }
     
-    // start search with sum of zero in position zero:
+    // Begin search
     return search(0,0);
   }
   
+  // Call recursive search function and hope for a solution!
   return lookForSolution() || false; 
-         
 }
